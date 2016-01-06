e0cac5b4e752867c44ef8ca35815c5584d46ed9e
duzliang <duzliang@gmail.com>
2016-01-06 09:19:19 +0800

    add js note

---
+++ javascript/es5/centence.js
@@ -0,0 +1,12 @@
+var arr = [1,3,5,7,9];
+
+for (var i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+for (var a in object) {
+  if (object.hasOwnProperty(a)) {
+    console.log('true');
+  }
+}
+
