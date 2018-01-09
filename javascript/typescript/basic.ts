f19bc3bc3ee2e70d29f0e61363d132f1d749c404
duzliang <duzliang@gmail.com>
2018-01-09 14:43:01 +0800

    typescript begin

---
+++ javascript/typescript/basic.ts
@@ -0,0 +1,26 @@
+// class
+class Student {
+    fullName: string;
+
+    constructor(
+        public firstName: string,
+        public middleInitial: string,
+        public lastName: string
+    ) {
+        this.fullName = firstName + ' ' + middleInitial + ' ' + lastName
+    }
+}
+
+// interface
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person) {
+    return 'Hello, ' + person.firstName + ' ' + person.lastName;
+}
+
+let user = new Student('Jane', 'M.', 'User');
+
+document.body.innerHTML = greeter(user);
