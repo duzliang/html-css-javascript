a072e391a0ace7208a55503b23acfa9cd68eadb7
{duke} <duzliang@gmail.com>
2022-12-21 18:02:15 +0800

    ts: type manipulation

--- javascript/typescript/type-manipulation.ts
+++ javascript/typescript/type-manipulation.ts
@@ -48,6 +48,7 @@ myGenericNum.add = function (x, y) {
 
 /**
  * keyof
+ * object key type
  */
 type Point = { x: number; y: number };
 type p = keyof Point;
@@ -60,6 +61,7 @@ type M = keyof Mapish;
 
 /**
  * typeof
+ * use on identities and their property
  */
 let s = 'hello';
 let n: typeof s;
@@ -68,7 +70,52 @@ type Predicate = (x: unknown) => boolean;
 type K = ReturnType<Predicate>;
 
 function f() {
-  return { x: 10, y: 3};
+  return { x: 10, y: 3 };
 }
 type P = ReturnType<typeof f>;
+
+/**
+ * Indexed Access Types
+ * 
+ * use indexed access type to look up a specific property on another type
+ */
+type Person = { name: string; age: number; alive: boolean };
+type Age = Person['age'];
+
+type I1 = Person['name' | 'age'];
+type I2 = Person[keyof Person];
+
+type aliveOrName = 'alive' | 'name';
+type I3 = Person[aliveOrName];
+
+
+const MyArray = [
+  { name: 'Alice', age: 20 },
+  { name: 'Bob', age: 23 },
+  { name: 'Evli', age: 28 },
+];
+
+type Person2 = typeof MyArray[number];
+type pAge = typeof MyArray[number]['age'];
+type pAge2 = Person2['age'];
+
+// const key2 = 'age'; // wrong
+type key = 'age';
+type page22 = Person2[key];
+
+
+/**
+ * Template Literal Type
+ */
+type World = 'world';
+type Greeting = `hello ${ World }`;
+
+type EmailLocalIDs = 'welcome_email' | 'email_handing';
+type FooterLocaleIDs = "footer_title" | "footer_sendoff";
+
+type AllLocaleIDs = `${EmailLocalIDs | FooterLocaleIDs}_id`;
+
+type Lang = "en" | "ja" | "pt";
+ 
+type LocaleMessageIDs = `${Lang}_${AllLocaleIDs}`;
 
