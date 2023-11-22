798429787d647adf98e175e6ad55af13b18a4ff2
Duke <duzliang@gmail.com>
2023-11-22 22:50:17 +0800

    object prototype

--- javascript/es5/prototype/index.js
+++ javascript/es5/prototype/index.js
@@ -20,4 +20,36 @@
   myObject.a; // 3
 
   myObject.hasOwnProperty('a'); // true
+});
+
+/**
+ * prototype extend
+ */
+(function() {
+  function Foo(name) {
+    this.name = name;
+  }
+
+  Foo.prototype.myName = function() {
+    return this.name;
+  }
+
+  function Bar(name, label) {
+    Foo.call(this, name);
+    this.label = label;
+  }
+  console.log('log=>Foo.prototype.constructor:', Foo.prototype.constructor);
+
+  // 创建一个新的Bar.prototype并关联到Foo.prototype
+  // todo why ydkjs says: Bar.prototype has no constructor
+  Bar.prototype = Object.create(Foo.prototype);
+  console.log('log=>Bar constructor:', Bar.prototype.constructor);
+
+  Bar.prototype.myLabel = function () {
+    return this.label;
+  }
+
+  var a = new Bar('duke', 'coder');
+  console.log('log=>name', a.myName());
+  console.log('log=>label', a.myLabel());
 })();
