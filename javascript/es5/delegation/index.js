470b802c558b5ffeade8b1bf6c1978558007a111
Duke <duzliang@gmail.com>
2023-11-24 23:27:11 +0800

    js delegation: parent vs class

--- javascript/es5/delegation/index.js
+++ javascript/es5/delegation/index.js
@@ -30,7 +30,7 @@
   console.log('log=>a1.constructor', a1.constructor);
   console.log('log=>a1.constructor.name', a1.constructor.name);
 
-  Foo.prototype.constructor = function Gotcha(){};
+  Foo.prototype.constructor = function Gotcha() {};
   console.log('log=>a1.constructor', a1.constructor); // Gotcha() {}
   console.log('log=>a1.constructor.name', a1.constructor.name); // "Gotcha"
   console.log('log=>a1', a1); // Foo {}
@@ -41,7 +41,7 @@
 
   Object.defineProperty(Bar, 'constructor', {
     enumerable: false,
-    value: function Gotcha(){},
+    value: function Gotcha() {},
   });
   console.log('log=>b1', b1); // Gotcha {}
 });
@@ -100,4 +100,106 @@
 
   b1.speak();
   b2.speak();
+});
+
+/**
+ * 案例1
+ * 类风格代码
+ */
+(function() {
+  // 父类
+  function Widget(width, height) {
+    this.width = width || 50;
+    this.height = height || 50;
+    this.$elem = null;
+  }
+
+  Widget.prototype.render = function($where) {
+    if (this.$elem) {
+      this.$elem.css({
+        width: this.width + 'px',
+        height: this.height + 'px',
+      }).appendTo($where);
+    }
+  };
+
+  // 子类
+  function Button(width, height, label) {
+    // 调用“super”构造函数
+    Widget.call(this, width, height);
+    this.label = label || 'Default';
+
+    this.$elem = $('<button>').text(this.label);
+  }
+
+  // 让Button“继承”Widget
+  Button.prototype = Object.create(Widget.prototype);
+
+  // 重写render(..)
+  Button.prototype.render = function($where) {
+    // “super”调用
+    Widget.prototype.render.call(this, $where);
+    this.$elem.click(this.onClick.bind(this));
+  };
+
+  Button.prototype.onClick = function(evt) {
+    console.log('Button \'' + this.label + '\' clicked!');
+  };
+
+  $(document).ready(function() {
+    let $body = $(document.body);
+    let btn1 = new Button(125, 30, 'Hello');
+    let btn2 = new Button(150, 40, 'World');
+
+    btn1.render($body);
+    btn2.render($body);
+  });
+});
+
+/**
+ * 案例2：使用class实现
+ */
+(function() {
+  class Widget {
+    constructor(width, height) {
+      this.width = width || 50;
+      this.height = height || 50;
+      this.$elem = null;
+    }
+
+    render($where) {
+      if (this.$elem) {
+        this.$elem.css({
+          width: this.width + 'px',
+          height: this.height + 'px',
+        }).appendTo($where);
+      }
+    }
+  }
+
+  class Button extends Widget {
+    constructor(width, height, label) {
+      super(width, height);
+      this.label = label || 'Default';
+      this.$elem = $('<button>').text(this.label);
+    }
+
+    render($where) {
+      super.render($where);
+      this.$elem.click(this.onClick.bind(this));
+    }
+
+    onClick(evt) {
+      console.log('Button \'' + this.label + '\' clicked!');
+    }
+  }
+
+  $(document).ready(function() {
+    let $body = $(document.body);
+    let btn1 = new Button(125, 30, 'Hello');
+    let btn2 = new Button(150, 40, 'World');
+
+    btn1.render($body);
+    btn2.render($body);
+  });
 })();
