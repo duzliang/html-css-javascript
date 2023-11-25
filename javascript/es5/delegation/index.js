6fdc3f8ab80d741f5347c5fd29d849042496052d
Duke <duzliang@gmail.com>
2023-11-25 11:19:56 +0800

    js delegation: inheritance vs composition

--- javascript/es5/delegation/index.js
+++ javascript/es5/delegation/index.js
@@ -104,7 +104,7 @@
 
 /**
  * 案例1
- * 类风格代码
+ * 类风格代码：模拟类继承
  */
 (function() {
   // 父类
@@ -157,7 +157,8 @@
 });
 
 /**
- * 案例2：使用class实现
+ * 案例1
+ * 使用class实现
  */
 (function() {
   class Widget {
@@ -184,6 +185,7 @@
       this.$elem = $('<button>').text(this.label);
     }
 
+    // 伪多态，重写父类的方法
     render($where) {
       super.render($where);
       this.$elem.click(this.onClick.bind(this));
@@ -202,4 +204,223 @@
     btn1.render($body);
     btn2.render($body);
   });
+});
+
+/**
+ * 案例2
+ * 使用委托设计模式
+ * 对象关联可以更好地支持关注分离（separation of concerns）原则
+ */
+(function() {
+  let Widget = {
+    init: function(width, height) {
+      this.width = width || 50;
+      this.height = height || 50;
+      this.$elem = null;
+    },
+    insert: function($where) {
+      if (this.$elem) {
+        this.$elem.css({
+          width: this.width + 'px',
+          height: this.height + 'px',
+        }).appendTo($where);
+      }
+    },
+  };
+
+  let Button = Object.create(Widget);
+
+  Button.setup = function(width, height, label) {
+    // 委托调用
+    this.init(width, height);
+    this.label = label || 'Default';
+    this.$elem = $('<button>').text(this.label);
+  };
+  Button.build = function($where) {
+    // 委托调用
+    this.insert($where);
+    this.$elem.click(this.onClick.bind(this));
+  };
+  Button.onClick = function(evt) {
+    console.log('Button \'' + this.label + '\' clicked!');
+  };
+
+  $(document).ready(function() {
+    let $body = $(document.body);
+
+    let btn1 = Object.create(Button);
+    btn1.setup(125, 30, 'Hello');
+
+    let btn2 = Object.create(Button);
+    btn2.setup(150, 40, 'World');
+
+    btn1.build($body);
+    btn2.build($body);
+  });
+});
+
+/**
+ * 案例3
+ * 多组件情况
+ * 类设计模式
+ */
+(function() {
+  // 父类
+  function Controller() {
+    this.errors = [];
+  }
+
+  Controller.prototype.showDialog = function(title, msg) {
+    console.log('log=>showDialog:', title, msg);
+  };
+  Controller.prototype.success = function(msg) {
+    this.showDialog('Success', msg);
+  };
+  Controller.prototype.failure = function(err) {
+    this.errors.push(err);
+    this.showDialog('Error', err);
+  };
+
+  // 子类：登录控件
+  function LoginController() {
+    Controller.call(this);
+  }
+
+  LoginController.prototype = Object.create(Controller.prototype);
+  LoginController.prototype.getUser = function() {
+    return document.getElementById('login_username').value;
+  };
+  LoginController.prototype.getPassword = function() {
+    return document.getElementById('login_password').value;
+  };
+  LoginController.prototype.validateEntry = function(user, pwd) {
+    user = user || this.getUser();
+    pwd = pwd || this.getPassword();
+
+    if (!(user && pwd)) {
+      return this.failure('请输入用户名和密码');
+    } else if (pwd.length < 3) {
+      return this.failure('密码必须大于三位');
+    }
+    return true;
+  };
+  // rewrite failure()
+  LoginController.prototype.failure = function(err) {
+    // call super
+    Controller.prototype.failure.call(this, '登录失败' + err);
+  };
+
+  /**
+   * 子类：验证控件
+   */
+  function AuthController(login) {
+    Controller.call(this);
+    // 合成
+    // in addition to inheritance, we also need composition
+    this.login = login;
+  }
+
+  // 把子类关联到父类
+  AuthController.prototype = Object.create(Controller.prototype);
+  AuthController.prototype.server = function(url, data) {
+    return $.ajax({
+      url: url,
+      data: data,
+    });
+  };
+  AuthController.prototype.checkAuth = function() {
+    let user = this.login.getUser();
+    let pwd = this.login.getPassword();
+    console.log('log=>', user, pwd);
+
+    if (this.login.validateEntry(user, pwd)) {
+      // just for test
+      this.server('/check-auth', {
+        user,
+        pwd,
+      }).then(this.success.bind(this))
+        .fail(this.failure.bind(this));
+    }
+  };
+
+  // 重写基础的success()
+  AuthController.prototype.success = function() {
+    // super
+    Controller.prototype.success.call(this, '验证通过');
+  };
+  // 重写基础的failure()
+  AuthController.prototype.failure = function(err) {
+    // super
+    Controller.prototype.failure.call(this, '验证失败' + err);
+  };
+
+  let auth = new AuthController(
+    // 除了继承，我们还需要合成
+    new LoginController(),
+  );
+  auth.checkAuth();
+});
+
+/**
+ * 行为委托模式
+ */
+(function() {
+  const LoginController = {
+    errors: [],
+    getUser: function() {
+      return document.getElementById('login_username').value;
+    },
+    getPassword: function() {
+      return document.getElementById('login_password').value;
+    },
+    validateEntry: function(user, pwd) {
+      user = user || this.getUser();
+      pwd = pwd || this.getPassword();
+
+      if (!(user && pwd)) {
+        return this.failure('Please enter a username & password!');
+      } else if (pwd.length < 5) {
+        return this.failure('Password must be 5+ characters!');
+      }
+      return true;
+    },
+    showDialog: function(title, msg) {
+      this.showDialog('Success', 'Login success');
+    },
+    failure: function(err) {
+      this.errors.push(err);
+      this.showDialog('Error', 'Login invalid: ' + err);
+    },
+  };
+
+  // Link `AuthController` to delegate to `LoginController`
+  // 让AuthController委托LoginController
+  const AuthController = Object.create(LoginController);
+
+  AuthController.errors = [];
+  AuthController.checkAuth = function() {
+    let user = this.getUser();
+    let pwd = this.getPassword();
+
+    if (this.validateEntry(user, pwd)) {
+      this.server('/check-auth', {
+        user,
+        pwd,
+      })
+        .then(this.accepted.bind(this))
+        .fail(this.rejected.bind(this));
+    }
+  };
+  AuthController.server = function(url, data) {
+    return $.ajax({
+      url: url,
+      data: data,
+    });
+  };
+  AuthController.accepted = function() {
+    this.showDialog('Success', 'Authenticated!');
+  };
+  AuthController.rejected = function(err) {
+    this.failure('Auth Failed: ' + err);
+  };
 })();
