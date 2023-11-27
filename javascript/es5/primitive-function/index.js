d75fee53a8c1039aeca660688c1ca4665ea3c30a
杜志良 <duzhiliang@feewee.cn>
2023-11-27 18:19:54 +0800

    js primitive function

--- javascript/es5/primitive-function/index.js
+++ javascript/es5/primitive-function/index.js
@@ -50,4 +50,74 @@
   // 空数组
   var eptArr = new Array(3);
   console.log('log=>eptArr:', eptArr); // [empty × 3]
+});
+
+/**
+ * 强制类型转换
+ */
+(function() {
+  var a = 42;
+  var b = a + '';       // 隐式强制类型转换
+  var c = String(a);    // 显式强制类型转换
+  console.log('log=>a:', a);
+  console.log('log=>b:', b);
+  console.log('log=>c:', c);
+
+  /**
+   * ToString()
+   * 特殊值
+   * 1. null => 'null'
+   * 2. undefined => 'undefined'
+   * 3. boolean => 'true' or 'false'
+   */
+
+  /**
+   * JSON 字符串化
+   * JSON-safe JSON 安全字符串
+   * 1. 字符串
+   * 2. 数字
+   * 4. 数组
+   * 5. 对象
+   */
+  console.log('log=>', JSON.stringify(42));   // "42"
+  console.log('log=>', JSON.stringify('42')); // ""42""（含有双引号的字符串）
+  console.log('log=>', JSON.stringify(null)); // "null"
+  console.log('log=>', JSON.stringify(true)); // "true"
+
+  var a = {
+    b: 42,
+    c: '42',
+    d: [1, 2, 3],
+  };
+
+  var aJson = JSON.stringify(a, null, 3);
+  console.log('log=>aJson', aJson);
+  var bJson = JSON.stringify(a, null, '--');
+  console.log('log=>bJson', bJson);
+
+  /**
+   * ToNumber()
+   */
+  var n1 = {
+    valueOf: function() {
+      return '42';
+    },
+  };
+  var n2 = {
+    toString: function() {
+      return '42';
+    },
+  };
+  var n3 = [4, 2];
+  n3.toString = function() {
+    return this.join(''); // "42"
+  };
+
+  console.log('log=>', Number(n1));                // 42
+  console.log('log=>', Number(n2));                // 42
+  console.log('log=>', Number(n3));                // 42
+  console.log('log=>', Number(''));           // 0
+  console.log('log=>', Number([]));           // 0
+  console.log('log=>', Number(['abc']));      // NaN
+
 })();
