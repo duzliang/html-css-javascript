572621c9d9a8923838761dfcd7f86beb00013ffa
Duke <duzliang@gmail.com>
2023-12-16 14:01:35 +0800

    :mmo: tco

--- javascript/performance/benchmarking-tuning/index.js
+++ javascript/performance/benchmarking-tuning/index.js
@@ -1,3 +1,10 @@
+/**
+ * 工具网站
+ * benchmarking: https://benchmarkjs.com/
+ * jsperf.app: https://jsperf.app/
+ *
+ */
+
 (function() {
   console.log('log=>', console.time('for'));
   for (let i = 0; i < 10000; i++) {
@@ -15,5 +22,40 @@
     return n * factorial(n - 1);
   }
 
-  console.log('log=>', factorial(5));
+  console.log('log=>', factorial(10)); // 可能会被引擎直接优化为常量 120
+});
+
+/**
+ * 尾调用优化（Tail Call Optimization, TCO）
+ */
+(function() {
+  // 简单例子
+  function foo(x) {
+    return x;
+  }
+
+  function bar(y) {
+    return foo(y + 1); // 尾调用
+  }
+
+  function baz() {
+    return 1 + bar(40); // 非尾调用
+  }
+
+  baz();
+
+  // 应用
+  function factorial(n) {
+    function fact(n, res) {
+      if (n < 2) {
+        return res;
+      }
+      return fact(n - 1, n * res);
+    }
+
+    return fact(n, 1);
+  }
+
+  let result = factorial(10);
+  console.log('log=>result', result);
 })();
